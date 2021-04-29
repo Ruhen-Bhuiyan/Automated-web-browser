ab4aba44d88919bf96fe1cf49659706254aba76b
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-04-29 23:03:35 +0600

    Add files via upload

---
+++ automated.py
@@ -0,0 +1,9 @@
+import webbrowser
+import pyautogui as pg
+import time
+webbrowser.open("https://www.bbc.com/news")
+time.sleep(10)
+pg.click(143,184)
+time.sleep(10)
+pg.click(226,184)
+time.sleep(10)
